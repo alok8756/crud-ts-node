7478724cf745cadef9bcc1666d4f0e0222a68f53
alok8756 <alokmaddheshiya0010@gmail.com>
2025-09-26 22:53:49 +0530

    fix: alok8756

--- src/core/database/db.config.ts
+++ src/core/database/db.config.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const MONGO_URI: string = process.env.MONGO_URI || '';
 
 if (!MONGO_URI) {
-  throw new Error('MONGO_URI is not defined in .env files alok');
+  throw new Error('MONGO_URI is not defined in .env files alok8756');
 }
 
 const connectDB = async () => {
