8f108a6aad0f5e76eb7b1304c403455eafdc7645
alokpw87 <alok.maddheshiya@pw.live>
2025-09-26 22:21:32 +0530

    fix: update the error message

--- src/core/database/db.config.ts
+++ src/core/database/db.config.ts
@@ -6,7 +6,7 @@ dotenv.config();
 const MONGO_URI: string = process.env.MONGO_URI || '';
 
 if (!MONGO_URI) {
-  throw new Error('MONGO_URI is not defined in .env file');
+  throw new Error('MONGO_URI is not defined in .env files');
 }
 
 const connectDB = async () => {
